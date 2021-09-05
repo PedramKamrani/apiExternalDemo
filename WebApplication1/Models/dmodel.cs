6570a8f11322742aa1a5ec25406c5b4a1e646860
PedramKamrani <kamranipedram@gmail.com>
2021-09-05 10:10:40 +0430

    apidemo

---
+++ WebApplication1/Models/dmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class dmodel
+    {
+        public string name { get; set; }
+        public string[] films { get; set; }
+        public string gravity { get; set; }
+    }
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public dmodel Dmodel { get; set; }
+        public IndexModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            using (var client=_clientFactory.CreateClient())
+            {
+                using (var res=await client.GetAsync("https://swapi.dev/api/planets/1"))
+                {
+                    var apires = await res.Content.ReadAsStringAsync();
+
+                    Dmodel = JsonConvert.DeserializeObject<dmodel>(apires);
+                    
+                }
+            }
+
+            return Page();
+        }
+    }
+}
